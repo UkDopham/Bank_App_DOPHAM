1cee86207a91f9773f25635a4836601639f50bed
UkDopham <alexandre34980@outlook.fr>
2021-02-26 22:46:28 +0100

    change file organization

--- app/src/main/java/com/example/bank_app_dopham/APIURL.java
+++
@@ -1,7 +0,0 @@
-package com.example.bank_app_dopham;
-
-public class APIURL {
-    private static String URL = "https://60102f166c21e10017050128.mockapi.io/labbbank";
-    public static String URL_CONFIG = URL+"/config/";
-    public static String URL_ACCOUNTS = URL+"/accounts"; //https://60102f166c21e10017050128.mockapi.io/labbbank/accounts"
-}
